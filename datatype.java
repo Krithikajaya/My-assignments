ef9bfc94de9aae191e1a4aa60e72e407ea9ac273
Krithikajaya <158828752+Krithikajaya@users.noreply.github.com>
2024-02-04 20:05:04 +0530

    Add files via upload
    day1 assignment

---
+++ datatype.java
@@ -0,0 +1,19 @@
+package assignment1;
+
+	public class datatype{
+		public static void main(String[] args) {
+			float version=23.4f;
+			String browsername="Chrome";
+			char logo='s';
+			int noofwindows=5;
+			boolean browseropen=true;
+			
+			System.out.println("the version is:"+version);
+			System.out.println("the browser namw is"+browsername);
+			System.out.println("the logo is:"+logo);
+			System.out.println("the no of windows opened is:"+noofwindows);
+			System.out.println("the browser open status is:"+browseropen);
+					
+		
+		}
+}
---
+++ ifcase.java
@@ -0,0 +1,20 @@
+package assignment1;
+
+public class ifcase {
+	public static void main(String[] args) {
+		int num=0;
+		if (num>0) {
+			System.out.println("the number is positive");
+			
+		} else if (num<0) {
+			System.out.println("the number is negative");
+			
+		} else {
+			System.out.println("the number is 0");
+
+		}
+
+		
+	}
+
+}
